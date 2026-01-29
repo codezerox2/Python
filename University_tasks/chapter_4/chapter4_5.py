bde60d6c5c18d7543644ec4b83b7d90489aecde0
codezerox2 <codezerox44@gmail.com>
2026-01-29 19:56:12 +0200

    Update chapter4_5.py

--- University_tasks/chapter_4/chapter4_5.py
+++ University_tasks/chapter_4/chapter4_5.py
@@ -1,5 +1,5 @@
 def Is_Even(num):
-    if num % 2 == 0 and num != 0: # remove and if isnot required
+    if num % 2 == 0 and num != 0:
         print("its even number")
     else:
         print('its not even number')
